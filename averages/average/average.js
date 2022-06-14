b03f4cf2a3bb73bec485f70bb78517b9e5fe9952
Marcos Veron <marcosveronar@gmail.com>
2022-06-14 04:38:22 -0300

    Creé la función mediaAritmetica en el archivo average.js

---
+++ averages/average/average.js
@@ -0,0 +1,15 @@
+// función para el metodo reduce
+const acumularSuma = (acumulador, numero) => acumulador + numero;
+
+//sumarLista recibe una lista de numeros que irá sumando por cada iteración en un nuevo array
+function sumarLista (array) {
+    let suma = array.reduce(acumularSuma)
+    return suma
+} 
+//mediaAritmetica utiliza la función sumarLista para recibir el array sumado y devolverá su media aritmetica
+
+function mediaAritmetica(array) {
+    let lista = sumarLista(array)
+    lista = lista/array.length
+    return lista
+}
